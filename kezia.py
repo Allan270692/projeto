74b053db14056e962301fcf632517c553a5aac52
lyamaria <lillyamaria980@gmail.com>
2025-05-20 17:05:51 -0300

    segundo commit

--- kezia.py
+++ kezia.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import streamlit as st
+from reportlab.pdfgen import canvas  #aqui estamos importando o tipo canvas
+from reportlab.lib.pagesizes import A4  #aqui o tamanho da página que irá aparecer no pdf criado
 
-tabela_aids = pd.read_csv("dados_aids_hiv.csv")
 
-tabela_aids.dropna()
+tabela_aids = pd.read_csv("dados_aids_hiv.csv")  #foi criado um arquivo csv com os dados que foram retirados do site do DATASUS sobre AIDS/HIV
+
+#e criamos uma váriavel que irá ler os dados do arquivo
+
+tabela_aids.dropna() #aqui é para retirar os dados nulos do arquivo
+
+
+cnv = canvas.Canvas("tabela_aids.pdf", pagesize=A4)
+
+cnv.save()
 
