44aa592b1d69c8daab79ceae62cacc5eb1e37691
lyamaria <lillyamaria980@gmail.com>
2025-06-02 23:26:29 -0300

    começo da limpeza de dados + arquivo ipynb pra facilitar visualização

--- kezia.py
+++ kezia.py
@@ -1,16 +1,15 @@
-import pandas as pd
-import streamlit as st
+
+import numpy as np
+import matplotlib.pyplot as plt
 from reportlab.pdfgen import canvas  #aqui estamos importando o tipo canvas
 from reportlab.lib.pagesizes import A4  #aqui o tamanho da página que irá aparecer no pdf criado
+import pandas as pd
 
+df = pd.read_excel("dados_aids_hiv_excel.xlsx")  #foi criado um arquivo excel com os dados que foram retirados do site do DATASUS sobre AIDS/
+df = df.dropna()
+df = df.drop_duplicates()
 
-tabela_aids = pd.read_csv("dados_aids_hiv.csv")  #foi criado um arquivo csv com os dados que foram retirados do site do DATASUS sobre AIDS/HIV
-
-#e criamos uma váriavel que irá ler os dados do arquivo
-
-tabela_aids.dropna() #aqui é para retirar os dados nulos do arquivo
-
-cnv = canvas.Canvas("tabela_aids.pdf", pagesize=A4) #começo da criação do pdf
+cnv = canvas.Canvas("grafico_aids.pdf", pagesize=A4) #começo da criação do pdf
 
 cnv.save()
 
